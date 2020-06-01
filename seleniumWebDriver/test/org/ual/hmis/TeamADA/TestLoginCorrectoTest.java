6d12a828cc9e14a2b40da34b4761fb06a9bdec9f
Dor494 <dor494@inlumine.ual.es>
2020-06-01 14:07:36 +0200

    Comentar los drivers

--- seleniumWebDriver/test/org/ual/hmis/TeamADA/TestLoginCorrectoTest.java
+++ seleniumWebDriver/test/org/ual/hmis/TeamADA/TestLoginCorrectoTest.java
@@ -33,7 +33,7 @@ public class TestLoginCorrectoTest {
   
   @Before
   public void setUp() {
-	  	System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+	  	// System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
 		// System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 	  	FirefoxOptions firefoxOptions = new FirefoxOptions();
 	  	firefoxOptions.setHeadless(true);
