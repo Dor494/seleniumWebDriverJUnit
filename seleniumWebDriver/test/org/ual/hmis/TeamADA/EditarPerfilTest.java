7b322513d3203c9a4ee41263e84984883331d3d2
Alejo Martín Arias Filippo <alejoemma@gmail.com>
2020-06-04 20:55:20 +0100

    Driver configuration updated
    The reason was a compatibility problem between my ubuntu machine and the windows machine of my friends.

--- seleniumWebDriver/test/org/ual/hmis/TeamADA/EditarPerfilTest.java
+++ seleniumWebDriver/test/org/ual/hmis/TeamADA/EditarPerfilTest.java
@@ -31,13 +31,17 @@ public class EditarPerfilTest {
   JavascriptExecutor js;
   @Before
   public void setUp() {
-	  System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
-	  FirefoxOptions firefoxOptions = new FirefoxOptions();
-	  firefoxOptions.setHeadless(true);
-	  driver = new FirefoxDriver();
-	  js = (JavascriptExecutor) driver;
-	  vars = new HashMap<String, Object>();
-  }
+	//System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+	//System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+	FirefoxOptions firefoxOptions = new FirefoxOptions();
+	firefoxOptions.setHeadless(true);
+	driver = new FirefoxDriver();
+	//ChromeOptions chromeOptions = new ChromeOptions();
+	//chromeOptions.setHeadless(true);
+	//driver = new ChromeDriver();
+	js = (JavascriptExecutor) driver;
+	vars = new HashMap<String, Object>();
+}
   @After
   public void tearDown() {
     driver.quit();
